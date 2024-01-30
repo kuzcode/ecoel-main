46b3996b0ff9864863d1ba71d91189214576032d
Nikita Kuzmin <nikita.kuzmaaa@gmail.com>
2024-01-30 20:47:31 +0300

    add half of the rules

--- app/(root)/rules/page.tsx
+++ app/(root)/rules/page.tsx
@@ -3,7 +3,17 @@ import Footer from "@/components/shared/Footer";
 async function Rules() {
 
   return (
-    <p>Правила форума Эко Эл</p>
+    <><p className="rulesTitle">Правила форума Эко Эл</p>
+    <div className="rulesDiv">
+      <p className="rulesP">1) Категорически запрещено: </p>
+      <ul className="subList">
+        <li><p className="rulesP">Обсуждать политику</p></li>
+        <li><p className="rulesP">Спамить, флудить, вести себя неадекватно</p></li>
+        <li><p className="rulesP">Притворяться другой личностью</p></li>
+      </ul>
+      <p className="rulesP">2) Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium, quam doloribus. Perferendis omnis necessitatibus exercitationem officiis saepe laborum voluptatum. Facere tempora, pariatur officiis porro ut minima totam vel obcaecati maiores.</p>
+      <p className="rulesP">3) Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium, quam doloribus. Perferendis omnis necessitatibus exercitationem officiis saepe laborum voluptatum. Facere tempora, pariatur officiis porro ut minima totam vel obcaecati maiores.</p>
+    </div></>
   );
 }
 
