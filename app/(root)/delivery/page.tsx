7134b73de5a1ac3d455f25e0a6bc2cf0e0cb0e88
Nikita Kuzmin <nikita.kuzmaaa@gmail.com>
2024-03-10 10:33:35 +0300

    removed import

--- app/(root)/delivery/page.tsx
+++ app/(root)/delivery/page.tsx
@@ -1,8 +1,46 @@
 "use client";import Link from'next/link';import Image from"next/image";
-export var items:string[]=[];
-function add(item:string){items.push(item);document.getElementById('basket')?.classList.add('activeBasket');console.log(items)}function delivery(){return(<><h1 className='head-text text-left'>ЭкоЭл Delivery</h1><p className='mt-3 text-base-regular text-light-2'>Доставим вкусную еду в любой уголок нашей школы</p><Link href='/delivery/basket' className='floatRight'><Image className="basket" id="basket" src='/assets/basket.svg' alt="basket" width={48} height={48}/></Link><div className="container"><h2 className='title2 head-text'>Сладости</h2><ul>
-<li><Image src="/twix.png"alt="Twix"className="img" width={180} height={135}/><h6>Twix Extreme <span>· 82г</span></h6><p>3,20 BYN</p><button onClick={()=>add('twix')}> Добавить</button></li>
-<li><Image src="/skittles.png"alt="Skittles"className="img" width={180} height={135}/><h6>Skittles <span>· 38г</span></h6><p>2,70 BYN</p><button onClick={()=>add('skittles')}> Добавить</button></li>
-</ul></div><div className="container"><h2 className='title2 head-text'>Напитки</h2><ul><li> <Image src="/cola.png" alt="Cola" className="img" width={180} height={135}/> <h6>Coca-cola <span>· 0,5л</span></h6><p>3,20 BYN</p><button onClick={()=>add('Coca-cola; ')}> Добавить</button></li></ul></div></>);
+import { useState } from 'react';
+
+
+var itemsList = [
+    {
+        title: 'Twix Xtra',
+        price: 3.3,
+        img: "twix",
+        amount: "82г",
+        added: 0
+    },
+    {
+        title: 'Skittles',
+        price: 2.8,
+        img: "skittles",
+        amount: "38г",
+        added: 0
+    }
+]
+
+
+function delivery() {
+const [items, setItems] = useState(
+    itemsList
+);
+
+function add() {
+    document.getElementById('basket')?.classList.add('activeBasket');
 }
+    
+return(<><h1 className='head-text text-left'>ЭкоЭл Delivery</h1>
+<p className='mt-3 text-base-regular text-light-2'>Доставим вкусную еду в любой уголок нашей школы</p>
+<Link href='/delivery/basket' className='floatRight'><Image className="basket" id="basket" src='/assets/basket.svg' alt="basket" width={48} height={48}/></Link>
+<div className="container"><h2 className='title2 head-text'>Сладости</h2>
+<ul>
+{
+    items.map(item => (
+        <li><Image src = {`/${item.img}.png`} alt={item.img} className="img" width={180} height={135}/><h6>{item.title} <span>· {item.amount}</span></h6><p>{item.price} BYN</p><button onClick={() => add()}> Добавить</button></li>
+    ))
+}
+</ul>
+</div></>
+
+)}
 export default delivery;
